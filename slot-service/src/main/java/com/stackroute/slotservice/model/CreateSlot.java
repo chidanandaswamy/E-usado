2f7cb89138bf9043775ff0e195452856c8c83477
bittu.kumar <bittu.kumar@globallogic.com>
2022-10-04 00:08:49 +0530

    created slot service layer-II

--- slot-service/src/main/java/com/stackroute/slotservice/model/CreateSlot.java
+++ slot-service/src/main/java/com/stackroute/slotservice/model/CreateSlot.java
@@ -16,32 +16,14 @@ public class CreateSlot {
 
     private String slotId;
     private String sellerName;
-//    private String sellerEmailId;
-//    private String buyerName;
-//    private String buyerEmailId;
-//    private Enum slotStatus;
-//    private Date date;
-//    private Timestamp startTime;
-//    private Timestamp endTime;
-//    private String description;
-
-
-
-
-
-//    private String slotId;
-//    private String
-//    private String sellerEmailId
-//    “slot_Id                   : “int”,-   primary key
-//“sellerEmailId”       : “String,
-//            “sellerName”          : “String”,
-//            “buyerEmailId”       : “String,
-//            “buyerName”          : “String”,
-//            “slotStatus”             : “Enum”
-//            “date”                     : “time”,
-//            “startTime”             : “TimeStamp”,
-//            “endTime”              : “TimeStamp”
-//            “description”           : “String”
+    private String sellerEmailId;
+    private String buyerName;
+    private String buyerEmailId;
+    private String slotStatus;
+    private Date date;
+    private Timestamp startTime;
+    private Timestamp endTime;
+    private String description;
 
 
 }

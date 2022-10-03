8271ee4aa52114915789e02b94d7f367a503fdbf
ganesh <k.prabhu@globallogic.com>
2022-10-03 16:03:04 +0530

    Added 5 basic apis

---
+++ product-service/src/main/java/com/stackroute/productservice/model/Product.java
@@ -0,0 +1,36 @@
+package com.stackroute.productservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.UUID;
+
+@Document(collection = "product")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Product {
+
+    @Id
+    private UUID id;
+    private String productName;
+    private BigDecimal productPrice;
+    private String productBrand;
+    private String productCategory;
+    private String productDescription;
+    private HashMap<String, String> productSpecs;
+    private String productManufacturedYear;
+    private Long productPurchasedDate;
+    private String productOwnerEmail;
+    private Float productDiscount;
+    private Boolean warrantyStatus;
+    private Float productDamageLevel;
+
+}

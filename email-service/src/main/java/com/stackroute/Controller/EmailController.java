615c0c9e9e51c6ec60b6ea89e6dcb5a5e962ae16
chidananda.swamy <chidananda.swamy@globallogic.com>
2022-10-04 15:22:43 +0530

    Created Email sending service

--- email-service/src/main/java/com/stackroute/Controller/EmailController.java
+++ email-service/src/main/java/com/stackroute/Controller/EmailController.java
@@ -1,6 +1,6 @@
 //package com.stackroute.Controller;
 //
-//import com.stackroute.service.EmailService;
+
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.stereotype.Controller;
 //import org.springframework.ui.ModelMap;

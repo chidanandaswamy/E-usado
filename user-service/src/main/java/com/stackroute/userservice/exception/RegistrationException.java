53e98dce4fbeae0a402eaa0ac0e64b77fcf50e75
Ashutosh Panigrahy <ashutosh.panigrahy@globallogic.com>
2022-10-06 17:20:50 +0530

    updated userservice on 06.10.2022

---
+++ user-service/src/main/java/com/stackroute/userservice/exception/RegistrationException.java
@@ -0,0 +1,2 @@
+package com.stackroute.userservice.exception;public class RegistrationException {
+}
---
+++ user-service/src/main/java/com/stackroute/userservice/model/DbSequence.java
@@ -0,0 +1,7 @@
+package com.stackroute.userservice.model;
+
+import lombok.Data;
+
+@Data()
+public class DatabaseSequence {
+}

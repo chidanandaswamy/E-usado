4f1a098bac190f787a15ec07867d43ddcfc45612
user.name <dinesh.adik@globallogic.com>
2022-10-07 14:33:39 +0530

    updated from cart

--- cart-service/src/main/java/com/stackroute/cartservice/exception/CartNotFoundException.java
+++ cart-service/src/main/java/com/stackroute/cartservice/exception/CartNotFoundException.java
@@ -1,2 +1,14 @@
-package com.stackroute.cartservice.exception;public class CartNotFound {
+package com.stackroute.cartservice.exception;
+
+import java.util.NoSuchElementException;
+
+public class CartNotFoundException extends NoSuchElementException {
+    public CartNotFoundException() {
+
+    }
+
+    public CartNotFoundException(String s) {
+        super(s);
+    }
+
 }

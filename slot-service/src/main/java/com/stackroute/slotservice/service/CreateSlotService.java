3a5df010c12f2c3cff48398f66b85af6a918cf3a
bittu.kumar <bittu.kumar@globallogic.com>
2022-10-10 19:16:43 +0530

    swagger, exception handling in created slot  updated

--- slot-service/src/main/java/com/stackroute/slotservice/service/CreateSlotService.java
+++ slot-service/src/main/java/com/stackroute/slotservice/service/CreateSlotService.java
@@ -3,7 +3,8 @@ package com.stackroute.slotservice.service;
 import com.stackroute.slotservice.model.CreateSlot;
 import org.springframework.http.ResponseEntity;
 
-import java.util.UUID;
+import java.util.HashSet;
+
 
 
 public interface CreateSlotService {
@@ -11,16 +12,19 @@ public interface CreateSlotService {
 
 
 
-    ResponseEntity<?> getAllSlots();
+   // ResponseEntity<?> getAllSlots();
+    HashSet<CreateSlot> findAllUsers();
 
    // ResponseEntity<CreateSlot> getSlotById(Long id);
 
-    ResponseEntity<CreateSlot> getSlotById(Long id);
+//    ResponseEntity<CreateSlot> getSlotById(long slotId);
+ CreateSlot getById(long slotId);
 
     ResponseEntity<String> addCreateSlot(CreateSlot createSlot);
 
-    ResponseEntity<String> updateSlotById(Long id, CreateSlot createSlot);
+//    ResponseEntity<String> updateSlotById(long slotId, CreateSlot createSlot);
+ CreateSlot UpdateSlotById(CreateSlot createSlot, long slotId);
 
     void deleteAllCreateSlots();
-    ResponseEntity<String> deleteCreatedSlotById(Long id);
+    ResponseEntity<String> deleteCreatedSlotById(long slotId);
 }

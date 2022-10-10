94433f7f7c862d0e83d446e1658210aa42edc33f
chidananda.swamy <chidananda.swamy@globallogic.com>
2022-10-10 14:24:49 +0530

    Email sending service with attachment

--- email-service/src/main/java/com/stackroute/EmailServiceApplication.java
+++ email-service/src/main/java/com/stackroute/EmailServiceApplication.java
@@ -1,24 +1,25 @@
 package com.stackroute;
 
-import com.stackroute.service.EmailServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-
-import javax.mail.MessagingException;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class EmailServiceApplication {
-@Autowired
-private EmailServiceImpl service;
-	public static void main(String[] args) {
-		SpringApplication.run(EmailServiceApplication.class, args);
-	}
 
-	@EventListener(ApplicationReadyEvent.class)
-	public void triggerMail() throws MessagingException {
-		service.sendMail("chidanandaswamy356@gmail.com", "E-usado order conformation mail" ,"Thank for Ordering Using E-usado" ,"/home/chidananda/Downloads/windows-11-landscape-scenery-sunrise-stock-day-light-3840x2400-5661.jpg");
-	}
+//	@Autowired
+//	private EmailService service;
+
+//	@PostMapping("/sendingEmail")
+//	public MailResponse sendEmail(@RequestBody MailRequest request) {
+//		Map<String, Object> model = new HashMap<>();
+//		model.put("name", request.getName());
+//		model.put("location", "Banglore, India");
+//
+//		return service.sendEmail(request, model);
+//	}
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(EmailFreemarkerApplication.class, args);
+//	}
 }

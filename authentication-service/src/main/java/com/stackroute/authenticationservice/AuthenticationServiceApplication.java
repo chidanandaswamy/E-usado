104b98f8e5b9231a6b09ea7bf154af7d10b3f778
bittu.kumar <bittu.kumar@globallogic.com>
2022-10-13 13:54:31 +0530

    updated eureka server

--- authentication-service/src/main/java/com/stackroute/authenticationservice/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/com/stackroute/authenticationservice/AuthenticationServiceApplication.java
@@ -4,8 +4,9 @@ package com.stackroute.authenticationservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
+@EnableEurekaClient
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class AuthenticationServiceApplication {
 
--- email-service/src/main/java/com/stackroute/EmailServiceApplication.java
+++ email-service/src/main/java/com/stackroute/EmailServiceApplication.java
@@ -6,6 +6,7 @@ import com.stackroute.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import java.util.Map;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
 public class EmailServiceApplication {
 
 	@Autowired

a2b0d5a3f2757558eb5979f89a8e3968ba187da2
ganesh <k.prabhu@globallogic.com>
2022-10-17 22:32:20 +0530

    added service level test cases

--- product-service/src/test/java/com/stackroute/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/stackroute/productservice/ProductServiceApplicationTests.java
@@ -1,24 +1,46 @@
 package com.stackroute.productservice;
 
+import com.stackroute.productservice.controller.ProductController;
 import com.stackroute.productservice.model.Product;
 import com.stackroute.productservice.repository.ProductRepository;
 import com.stackroute.productservice.service.ProductService;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
 class ProductServiceApplicationTests {
+
+	private MockMvc mockMvc;
 
 	@Autowired
 	private ProductService productService;
 
-	@MockBean
+	@Mock
 	private ProductRepository productRepository;
+
+	@InjectMocks
+	private ProductController productController;
+
+
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Before
+	public void setUp(){
+		MockitoAnnotations.initMocks(this);
+		this.mockMvc = MockMvcBuilders.standaloneSetup(productController).build();
 	}
 
 }

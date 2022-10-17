2688bff506f28911543420008b91008b97942340
niit <mohit.2.sharma@stackroute.in>
2022-10-17 08:35:31 +0530

    new code

--- product-service/src/main/java/com/stackroute/productservice/service/ProductService.java
+++ product-service/src/main/java/com/stackroute/productservice/service/ProductService.java
@@ -4,11 +4,8 @@ import com.stackroute.productservice.model.Product;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
-<<<<<<< HEAD
-=======
 import java.math.BigDecimal;
 import java.util.List;
->>>>>>> 9338046489c5cb12b492b6a216f67fc61f6f708a
 import java.util.UUID;
 
 public interface ProductService {

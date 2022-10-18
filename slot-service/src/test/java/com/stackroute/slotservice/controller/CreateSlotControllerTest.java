c2b27bf63a06311714561e4a8abffb714d1dba78
bittu.kumar <bittu.kumar@globallogic.com>
2022-10-18 09:29:21 +0530

    updated testing part

---
+++ slot-service/src/test/java/com/stackroute/slotservice/controller/CreateSlotControllerTest.java
@@ -0,0 +1,18 @@
+package com.stackroute.slotservice.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class CreateSlotControllerTest {
+
+
+    @Test
+    public void demoTestMethod(){
+        //assertTrue( condition: true);
+    }
+
+
+
+
+
+
+}

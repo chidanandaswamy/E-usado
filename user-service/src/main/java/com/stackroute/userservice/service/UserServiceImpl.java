9d7492ff224c3b74711726ea4adab0ececbf3f23
Ashutosh Panigrahy <ashutosh.panigrahy@globallogic.com>
2022-10-18 15:07:19 +0530

    updated on 18.10.2022

--- user-service/src/main/java/com/stackroute/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/stackroute/userservice/service/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
+
 @Service
 public class UserServiceImpl implements UserService{
     @Autowired
@@ -70,6 +71,9 @@ public class UserServiceImpl implements UserService{
         else{throw new UserNotFoundException("User with email " + user + " doesn't exist.");}
 
     }
+
+
+
 
 }
 

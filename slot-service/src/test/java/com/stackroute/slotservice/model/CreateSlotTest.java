05b05a30fe6b1db08ddf477c7962b687d3a8296b
bittu.kumar <bittu.kumar@globallogic.com>
2022-10-20 14:53:26 +0530

    upadted

--- slot-service/src/test/java/com/stackroute/slotservice/model/CreateSlotTest.java
+++ slot-service/src/test/java/com/stackroute/slotservice/model/CreateSlotTest.java
@@ -16,8 +16,6 @@ public class CreateSlotTest {
     public  void setUp() throws Exception {
         createSlot = new CreateSlot();
 
-        //dateTimeSlots=new ArrayList<>();
-
         createSlot.setSellerName("bittu");
         createSlot.setSellerEmailId("bittu@gmail.com");
         createSlot.setBuyerName("Raju");
@@ -38,34 +36,3 @@ public class CreateSlotTest {
 
 
 
-//package com.stackroute.keepnote.test.model;
-//
-//        import java.util.Date;
-//        import org.junit.Before;
-//        import org.junit.Test;
-//        import org.meanbean.test.BeanTester;
-//
-//        import com.stackroute.keepnote.model.User;
-//
-//public class UserTest {
-//
-//    private User user;
-//
-//    @Before
-//    public void setUp() throws Exception {
-//
-//        user = new User();
-//        user.setUserId("Jhon123");
-//        user.setUserName("Jhon Simon");
-//        user.setUserPassword("123456");
-//        user.setUserMobile("9898989898");
-//        user.setUserAddedDate(new Date());
-//
-//    }
-//
-//    @Test
-//    public void test() {
-//        new BeanTester().testBean(User.class);
-//    }
-//
-//}
--- slot-service/src/test/java/com/stackroute/slotservice/service/CreateSlotServiceImpTest.java
+++ slot-service/src/test/java/com/stackroute/slotservice/service/CreateSlotServiceImpTest.java
@@ -1,5 +1,39 @@
 package com.stackroute.slotservice.service;
 
+import com.stackroute.slotservice.model.CreateSlot;
+import org.junit.Before;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.UUID;
+
 public class CreateSlotServiceImpTest {
+
+    @MockBean
+    private CreateSlot createSlot;
+
+
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+
+       // location = new Location("mangalore", new Double[]{-88.00, -90.90});
+
+        createSlot = new CreateSlot(
+
+        );
+
+
+
+
+
+    }
+
+
 
 }

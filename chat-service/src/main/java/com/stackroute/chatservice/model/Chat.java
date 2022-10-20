13636b556834043d65ac7822e66d7fa611e4e123
abhilash <abhilash.chaudhari@globallogic.com>
2022-10-20 18:20:31 +0530

    updated chat and config service

--- chat-service/src/main/java/com/stackroute/chatservice/model/Chat.java
+++ chat-service/src/main/java/com/stackroute/chatservice/model/Chat.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
@@ -15,7 +16,8 @@ import java.util.List;
 @Document(collection="chat")
 public class Chat {
 
-
+   @Transient
+    public static final String SEQUENCE_NAME = "booking_sequence";
     @Id
     private long questionId;
     private long productId;

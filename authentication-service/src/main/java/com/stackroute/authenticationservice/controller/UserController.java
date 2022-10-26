97dd9ef5b9b2f2f9d6a1961a5bca99eeee7327d7
azharuddin.n <azharuddin.n@globallogic.com>
2022-10-26 18:51:34 +0530

    swagger working fine

--- authentication-service/src/main/java/com/stackroute/authenticationservice/controller/UserController.java
+++ authentication-service/src/main/java/com/stackroute/authenticationservice/controller/UserController.java
@@ -6,6 +6,7 @@ import com.stackroute.authenticationservice.exception.UserAlreadyExistsException
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.*;
 
 import com.stackroute.authenticationservice.domain.User;
@@ -30,7 +31,20 @@ public class UserController {
         this.securityTokenGenerator = securityTokenGenerator;
     }
 
+    /*@GetMapping("/login")
+    public String index() {
+        OAuth2User user = getCurrentUser();
+        return "Hello " + user.getAttributes().get("name") + ". Your email is " + user.getAttributes().get("email")
+                + " and your profile picture is <img src='"+user.getAttributes().get("picture")+"' /> <br />"
+                + "<a href='/logout'>logout</a>";
+    }
 
+
+    public OAuth2User getCurrentUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return ((OAuth2AuthenticationToken)auth).getPrincipal();
+    }
+*/
     @PostMapping("/authenticate")
     public ResponseEntity<?> loginUser(@RequestBody User user) throws InvalidCredentialsException
     {
@@ -60,7 +74,7 @@ public class UserController {
         }else{
             throw new Exception("request is empty");
         }
-        return loggedInUser + "password has been Changed Successfully";
+        return loggedInUser + " "+ "password has been Changed Successfully";
     }
 
 

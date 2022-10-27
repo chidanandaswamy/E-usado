b6538cff48a094298c45799a161ff47c232089f4
azharuddin.n <azharuddin.n@globallogic.com>
2022-10-27 12:46:55 +0530

    added changes for pom

--- authentication-service/src/main/java/com/stackroute/authenticationservice/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/com/stackroute/authenticationservice/AuthenticationServiceApplication.java
@@ -3,9 +3,10 @@ package com.stackroute.authenticationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
-
+@EnableEurekaClient
 @SpringBootApplication
 public class AuthenticationServiceApplication {
 

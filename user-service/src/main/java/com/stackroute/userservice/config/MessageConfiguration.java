221ac5e370249fd152feca5df94b183f708d6724
chidananda.swamy <chidananda.swamy@globallogic.com>
2022-10-30 15:17:46 +0530

    pulling code 30 15:17

--- user-service/src/main/java/com/stackroute/userservice/config/MessageConfiguration.java
+++ user-service/src/main/java/com/stackroute/userservice/config/MessageConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -30,7 +29,7 @@ public class MessageConfiguration {
         return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
     }
     @Bean
-    public MessageConverter converter(){
+    public Jackson2JsonMessageConverter converter(){
         return new Jackson2JsonMessageConverter();
     }
     @Bean
